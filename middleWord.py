91420ddc4afccfba09e7d0486eed7b9467c86a79
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-04-13 08:49:00 -0400

    Updated from Brython Server: 4/13/2018 8:48:59 AM

--- middleWord.py
+++ middleWord.py
@@ -1,1 +1,13 @@
+#Shaylee McBride
+#13Apr2018
+#middleWord.py
 
+words = input('Enter some words: ').split(' ')
+
+middle = len(words)//2
+
+if middle == len(words)/2:
+    print(words[middle-1:middle+1])
+
+else:
+    print(words[middle])
