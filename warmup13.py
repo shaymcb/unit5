3ef6fb39e196e37c341df30d53a1e0c9e0be8c56
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-04-25 08:16:49 -0400

    Updated from Brython Server: 4/25/2018 8:16:48 AM

--- warmup13.py
+++ warmup13.py
@@ -1,1 +1,13 @@
+#Shaylee McBride
+#25Apr2018
+#warmup13.py - list of 20 random numbers and some stuff
 
+from random import randint
+
+alist = []
+for i in range(20):
+    alist.append(randint(1,100))
+
+print('Sum =',sum(alist))
+print('Min =',min(alist))
+print('Max =',max(alist))
