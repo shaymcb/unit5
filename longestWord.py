032698d162115317e3b2c5f95f9970064f483ca0
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-04-25 08:27:52 -0400

    Updated from Brython Server: 4/25/2018 8:27:51 AM

--- longestWord.py
+++ longestWord.py
@@ -1,1 +1,14 @@
+#Shaylee McBride
+#25Apr2018
+#longestWord.py
 
+words = input('Enter some words: ').split(' ')
+
+longest = 0
+for item in words:
+    if longest < len(item):
+        longest = len(item)
+        
+for item in words:
+    if len(item) == longest:
+        print(item)
