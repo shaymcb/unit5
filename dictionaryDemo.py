50ca3a053f017007408548433e8634b89033eae9
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-04-25 08:11:11 -0400

    Update dictionaryDemo.py

--- dictionaryDemo.py
+++ dictionaryDemo.py
@@ -1,1 +1,13 @@
+#Shaylee McBride
+#25Apr2018
+#dictionaryDemo.py - more list practice
 
+words = ['computer','mortify','dog','firetruck','yes','python','cat','attacking']
+
+words.sort()
+
+num = int(input('What number word do you want? '))
+if num<=0 or num>len(words):
+    print('Invalid Number')
+else:
+    print(words[num-1])
