c5d75c8434670b18bada91893ced926af584edbc
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-04-25 08:31:03 -0400

    Updated from Brython Server: 4/25/2018 8:31:02 AM

--- randomMonth.py
+++ randomMonth.py
@@ -1,1 +1,9 @@
+#Shaylee McBride
+#25Apr2018
+#randomMonth.py
 
+from random import randint
+
+months = ['January','February','March','April','May','June','July','August','September','October','November','December']
+
+print(months[randint(0,11)])
