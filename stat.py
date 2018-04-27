1a60b279802db34b1958d6bfa34c6a65f609b3f9
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-04-27 08:38:28 -0400

    Updated from Brython Server: 4/27/2018 8:38:28 AM

--- stat.py
+++ stat.py
@@ -1,1 +1,37 @@
+#Shaylee McBride
+#26Apr2018
+#stat.py
+
+print('Type a list of numbers')
+print('Enter q when done')
+L = []
+while True:
+    num = input("")
+    if num == 'q':
+        break
+    L.append(float(num))
+
+L.sort
+minimum = min(L)
+maximum = max(L)
+mean = sum(L)/len(L)
+
+middle = len(L)//2
+if middle == len(L)/2:
+    median = (L[middle-1] + L[middle])/2
+else:
+    median = L[middle]
+
+most = 0
+for item in L:
+    count = L.count(item)
+    if count > most:
+        most = count
+        mode = item
+
+print('Min =',minimum)
+print('Max =',maximum)
+print('Mean =',mean)
+print('Med =',median)
+print('Mode =',mode)
 
