0947be982413722523c3191fc97e5fc1bc02616f
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-04-27 08:32:26 -0400

    Updated from Brython Server: 4/27/2018 8:32:25 AM

--- matrixDemo.py
+++ matrixDemo.py
@@ -1,1 +1,18 @@
+#Shaylee McBride
+#27Apr2018
+#matrixDemo.py - lists inside lists
 
+def printBoard(board):
+    for r in range(3):
+        for c in range(3):
+            print(board[r][c],end=' ') #end changes seperation
+        print() #starts new line with nothing in it
+
+board = [['a','b','c'],['d','e','f'],['g','h','i']]
+printBoard(board)
+
+row = int(input('enter a row: '))
+col = int(input('enter a column: '))
+
+board[row-1][col-1] = 'X'
+printBoard(board)
