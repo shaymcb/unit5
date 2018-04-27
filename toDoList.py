303af460cfe3183df01ce51e9fbe551d3a133fbb
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-04-27 08:50:33 -0400

    Updated from Brython Server: 4/27/2018 8:50:33 AM

--- toDoList.py
+++ toDoList.py
@@ -1,1 +1,25 @@
+#Shaylee McBride
+#27Apr2018
+#toDoList.py
+
+def add(task):
+    toDo.append(task)
+
+def delete(task):
+    toDo.remove(task)
+
+toDo = []
+print('Valid commands = add, delete, print, quit')
+
+while True:
+    task = input('')
+    if task == 'quit':
+        break
+    elif task == 'print':
+        for item in toDo:
+            print(item)
+    task = task.split(' ')
+    
+    if 'add' in task:
+        toDo.append(task[1:])
 
