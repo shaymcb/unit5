f50ce75b6076630ee14f4f6075e62acb233179d7
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-04-30 08:23:42 -0400

    Updated from Brython Server: 4/30/2018 8:23:42 AM

--- warmup14.py
+++ warmup14.py
@@ -1,1 +1,10 @@
+#Shaylee McBride
+#30Apr2018
+#warmup14.py
 
+name = input('Enter your first and last name: ').split(' ')
+
+firstInitial = name[0][0]
+lastInitial = name[-1][0]
+
+print('Your initials are',firstInitial+'.',lastInitial+'.')
