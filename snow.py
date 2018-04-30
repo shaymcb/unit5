6c214d8dab832308b1fb440d3fb39b51115ed1f4
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-04-30 08:48:43 -0400

    Updated from Brython Server: 4/30/2018 8:48:42 AM

--- snow.py
+++ snow.py
@@ -1,1 +1,40 @@
+#Shaylee McBride
+#30Apr2018
+#snow.py
 
+from ggame import *
+from random import randint
+
+FLAKES = 2
+RATE = 20
+WIDTH = 1000
+HEIGHT = 540
+
+def step():
+    data['frames'] += 1
+    if data['frames']%RATE == 0:
+        for i in range(FLAKES):
+            data['snowList'].append(Sprite(data['flake'],(randint(0,WIDTH),0)))
+    for flake in data['snowList']:
+
+        if flake.y < HEIGHT:
+            flake.x += randint(-4,3)
+            flake.y += randint(0,3)
+
+if __name__ == "__main__":
+    
+    data = {}
+    data['snowList'] = []
+    data['frames'] = 0
+    data['accumulation'] = 0
+    
+    black = Color(0x000000,1)
+    white = Color(0xFFFFFF, 1)
+    background = RectangleAsset(WIDTH,HEIGHT,LineStyle(1,black),black)
+    data['flake'] = CircleAsset(10,LineStyle(1,white),white)
+    
+    Sprite(background)
+    for i in range(FLAKES):
+        data['snowList'].append(Sprite(data['flake'],(randint(0,WIDTH),0)))
+    
+    App().run(step)
