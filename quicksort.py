8f14e9d431c01e4af3b49021c281202df8d67fce
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-05-03 08:24:40 -0400

    Update quicksort.py

--- quicksort.py
+++ quicksort.py
@@ -1,1 +1,48 @@
+#Shaylee McBride
+#5/1/18
+#sortingTemplate.py - times a sorting function
+
+from random import randint
+from time import time
+
+N = 10 #how many numbers will be sorted
+
+def mySort(A):
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(len(A)-1):
+            if A[i] > A[i+1]:
+                A[i],A[i+1] = A[i+1],A[i] #swap in python
+                swapped = True
+        if not swapped:
+            break
+        swapped = False
+        for i in range(len(A)-2,-1,-1): #the second -1 tells it to go backwards
+            if A[i] > A[i+1]:
+                A[i],A[i+1] = A[i+1],A[i] #swap in python
+                swapped = True        
+    return A
+    
+if __name__ == '__main__':
+
+    #make a list of N random numbers between 1 and N
+    numbers = [0]*N
+    for i in range(N):
+        numbers[i] = randint(1,N)
+    
+    pythonSort = sorted(numbers) #Python's sort
+    
+    #time how long your sort takes
+    t1 = time()
+    numbers = mySort(numbers)
+    #numbers = numbers.sort()
+    t2 = time()
+    
+    #print whether the sort worked or not
+    try:
+        assert(numbers == pythonSort)
+        print('Your sort took', t2-t1, 'seconds')
+    except:
+        print('Your sort did not work')
 
