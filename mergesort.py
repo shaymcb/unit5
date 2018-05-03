ced2e633c0ad9540d14088a616558f1dbf93b643
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-05-03 08:57:48 -0400

    Updated from Brython Server: 5/3/2018 8:57:46 AM

--- mergesort.py
+++ mergesort.py
@@ -1,1 +1,40 @@
+#Shaylee McBride
+#5/1/18
+#sortingTemplate.py - times a sorting function
+
+from random import randint
+from time import time
+
+N = 100 #how many numbers will be sorted
+
+def mergesort(A,B,n):
+    B = A
+    splitmerge(B,0,n,A)
+
+splitmerge(B,iBegin,iEnd,A):
+    if iEnd - iBegin < 2:
+        return A
+    
+if __name__ == '__main__':
+
+    #make a list of N random numbers between 1 and N
+    numbers = [0]*N
+    for i in range(N):
+        numbers[i] = randint(1,N)
+    
+    pythonSort = sorted(numbers) #Python's sort
+    
+    #time how long your sort takes
+    t1 = time()
+    numbers = quicksort(numbers, 0, len(numbers)-1)
+    #numbers = numbers.sort()
+    t2 = time()
+    
+    #print whether the sort worked or not
+    try:
+        assert(numbers == pythonSort)
+        print('Your sort took', t2-t1, 'seconds')
+    except:
+        print('Your sort did not work')
+
 
