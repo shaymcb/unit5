692c5f5b822779a920cd0270ac00c3aeb98d43ab
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-05-07 08:21:55 -0400

    Updated from Brython Server: 5/7/2018 8:21:54 AM

--- quiz5.py
+++ quiz5.py
@@ -1,1 +1,31 @@
+#Shaylee McBride
+#7May2018
+#quiz5.py
 
+def penultimate(L):
+    return L[-2]
+
+def plusEquals(numbers, integer):
+    for i in range(len(numbers)):
+        numbers[i] = numbers[i]+integer
+    return numbers
+  
+def smallest(L):
+    minimum = L[0]
+    for num in L:
+        if num < minimum:
+            minimum = num
+    return minimum
+
+def decimalRange(small, large, count):
+    rangeList = []
+    num = small
+    while num < large:
+        rangeList.append(round(num,4))
+        num += count
+    return rangeList
+
+print(penultimate([1,2,3,'yes',5]))
+print(plusEquals([1,2,3,4,5],2))
+print(smallest([1,2,3,-3,5]))
+print(decimalRange(0.1,2.3,0.2))
